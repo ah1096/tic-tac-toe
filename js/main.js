52864e99a84fa59b48617d3ea6b94e0c43784805
ah1096 <alyssa.holbert@student.awesomeinc.org>
2022-10-06 16:14:05 +0000

    returned to square 1; all horiz. wins check out

--- js/main.js
+++ js/main.js
@@ -8,20 +8,9 @@ let boardState = [
 ];
 
 let winConditions = [
-
-    //HORIZONTAL//
     [0,1,2],
     [3,4,5],
     [6,7,8]
-
-    //VERTICAL//
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-
-    //DIAGONAL//
-    [0,4,8],
-    [2,4,6]
 ]
 
 
@@ -33,16 +22,6 @@ for (let i = 0; i < winConditions.length; i++){
     console.log(`BOARDSTATE[${line[0]}]: `, boardState[line[0]]);
     console.log(`BOARDSTATE[${line[1]}]: `, boardState[line[1]]);
     console.log(`BOARDSTATE[${line[2]}]: `, boardState[line[2]]);
-/////
-
-    console.log(`BOARDSTATE[${line[3]}]: `, boardState[line[3]]);
-    console.log(`BOARDSTATE[${line[4]}]: `, boardState[line[4]]);
-    console.log(`BOARDSTATE[${line[5]}]: `, boardState[line[5]]);
-
-    console.log(`BOARDSTATE[${line[6]}]: `, boardState[line[6]]);
-    console.log(`BOARDSTATE[${line[7]}]: `, boardState[line[7]]);
-  
-
 
     if (
         boardState[line[0]] 
@@ -53,29 +32,6 @@ for (let i = 0; i < winConditions.length; i++){
     } else {
         console.log('LOSE');
     }
-/////
-    if (
-        boardState[line[3]] 
-        && boardState[line[3]] == boardState[line[4]] 
-        && boardState[line[4]] == boardState[line[5]]
-    ) {
-        console.log('WIN');
-    } else {
-        console.log('LOSE');
-    }
-
-///
-if (
-    boardState[line[6]] 
-    && boardState[line[6]] == boardState[line[7]] 
-    && boardState[line[7]] == boardState[line[8]]
-) {
-    console.log('WIN');
-} else {
-    console.log('LOSE');
-}
-
-
 }
 
 
