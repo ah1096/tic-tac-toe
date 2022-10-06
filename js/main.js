1d183d90a248434ebade1cd199f1de216dd93af8
ah1096 <alyssa.holbert@student.awesomeinc.org>
2022-10-06 16:58:33 +0000

    horizontal wins WORKING

--- js/main.js
+++ js/main.js
@@ -8,9 +8,15 @@ let boardState = [
 ];
 
 let winConditions = [
+
+///HORIZONTAL///
     [0,1,2],
     [3,4,5],
     [6,7,8]
+
+
+
+
 ]
 
 
@@ -26,12 +32,34 @@ for (let i = 0; i < winConditions.length; i++){
     if (
         boardState[line[0]] 
         && boardState[line[0]] == boardState[line[1]] 
-        && boardState[line[1]] == boardState[line[2]]
-    ) {
-        console.log('WIN');
-    } else {
-        console.log('LOSE');
-    }
+        && boardState[line[1]] == boardState[line[2]]) 
+        {console.log('WIN');
+    } else if (
+        boardState[line[3]] 
+        && boardState[line[3]] == boardState[line[4]] 
+        && boardState[line[4]] == boardState[line[5]]) 
+        {console.log('WIN');
+    } else if (
+        boardState[line[6]] 
+        && boardState[line[6]] == boardState[line[7]] 
+        && boardState[line[7]] == boardState[line[8]]) 
+        {console.log('WIN');
+    } 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    else (console.log("LOSE"))
+    
+    
+    
+    
+    
 }
 
 
