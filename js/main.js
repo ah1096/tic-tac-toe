360fae59184670ee1b386069ec113d2c5274c0b5
ah1096 <alyssa.holbert@student.awesomeinc.org>
2022-10-06 15:12:29 +0000

    added working winconditions functions

--- js/main.js
+++ js/main.js
@@ -1,7 +1,32 @@
-let board = [ ['','',''],
-              ['','',''],
-              ['','',''],];
 
-function updateBoard(row, col){
-    
+
+//win conditions
+let boardState = [
+    'x','x','x',
+    '','','',
+    '','','',
+];
+
+let winConditions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8]
+]
+
+
+for (let i = 0; i < winConditions.length; i++){
+    const line = winConditions[i];
+
+
+    if (
+        boardState[line[0]] 
+        && boardState[line[0]] == boardState[line[1]] 
+        && boardState[line[1]] == boardState[line[2]]
+    ) {
+        console.log('WIN');
+    } else {
+        console.log('LOSE');
+    }
+
+
 }
